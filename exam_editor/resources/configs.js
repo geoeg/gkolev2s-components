722e2d42f131154256e7188937c54de5805f0619
Georgi Kolev <geoeg@rocketmail.com>
2019-11-24 22:30:15 +0100

    testing local and github file paths

--- exam_editor/resources/configs.js
+++ exam_editor/resources/configs.js
@@ -93,5 +93,101 @@ ccm.files[ 'configs.js' ] = {
 
     }
 
+  },
+
+  "localform" : {
+
+    // obligatory settings needed to run the form for creating an exam
+    "submit_settings": {
+
+      // structure of the exam form (general information and exercises)
+      // TODO: change file path to github.io..
+      "entries": [ "ccm.get", "./resources/datasets.js", "form.data" ],
+
+      // initial data for the exam form
+      "data": {
+        // TODO: change file path to github.io..
+        "store": [ "ccm.store", "./resources/datasets.js" ],
+        "key": "form_init"
+      },
+
+      // storage for the created exam (when the form is submitted)
+      "store": {
+        "settings": {
+          "name": "gkolev2s_exam_editor",
+          "url": "https://ccm2.inf.h-brs.de"
+        },
+        // "key": "%configurationKey%"
+      }
+
+    },
+
+    // user login settings (if no given, the default "guest" config will be loaded)
+    "user": [ 'ccm.instance','https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.3.0.js',
+    [ 'ccm.get','https://ccmjs.github.io/akless-components/user/resources/configs.js','hbrsinfkaul' ] ],
+    // [ 'ccm.get','https://ccmjs.github.io/akless-components/user/resources/configs.js','compact' ] ],
+
+    // those are additional settings for working with admin panel
+    "store_settings": {
+
+      // storage of original exams (parents)
+      "editor": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_editor",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // users with admin rights
+      "admins": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_admins",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // storage of the generated exams
+      "generator": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_generator",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // storage for the students allowed to participate an exam
+      "students": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_students",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // storage for the already unlocked exams
+      "unlocker": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_unlocker",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // storage for the results of the exams
+      "results": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_exam_results",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+      // storage for my own results (testing with student id: 9017419)
+      "myself": {
+        "store": [ "ccm.store", {
+          "name": "gkolev2s_9017419",
+          "url": "https://ccm2.inf.h-brs.de"
+        } ],
+      },
+
+    }
+
   }
+
 }
